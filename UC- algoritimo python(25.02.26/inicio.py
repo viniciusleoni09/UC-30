33d4ec124c7b82bc0a16d4af62a08e00f9944244
viniciusleoni09 <viniciusleonidc@gmail.com>
2026-02-25 17:14:36 -0300

    Add files via upload

---
+++ UC- algoritimo python(25.02.26/inicio.py
@@ -0,0 +1,9 @@
+nome = "gaby"
+idade = "17"
+altura = "1.90"
+matriculado = True
+
+print(nome)
+print(idade)
+print(altura)
+print(matriculado)
---
+++ UC- algoritimo python(25.02.26/variaveis.py
@@ -0,0 +1,6 @@
+var1 = 10
+var1 = 20
+var1 = 25
+
+print(var1)
+print(var1)
