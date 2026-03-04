404d58e2fd08aad6f7cd815a1af915efb3fa59e0
viniciusleoni09 <viniciusleonidc@gmail.com>
2026-03-04 08:33:33 -0300

    Add files via upload

---
+++ UC-ALGORITIMO(04,03,26)/listas.py
@@ -0,0 +1,30 @@
+#LISTAS
+
+nome1 = "ana"
+nome2 = "bruno"
+nome3 = "carlos"
+
+nomes = ["arthur","joao","maria"]
+print(nomes)
+
+dados = ["Gabriel", 0,1,60, True]
+print(dados)
+
+print(type(dados))
+print(type(dados[0]))
+print(type(dados[1]))
+print(type(dados[2]))
+print(type(dados[3]))
+
+alunos = ["Adriano","Barbara","Camila"]
+
+print("primeiro", alunos[0])
+print("primeiro", alunos[1])
+print("primeiro", len(alunos))
+print("primeiro", alunos[3])
+print("primeiro", len(alunos - 1))
+
+nomes = ["Ana","Bruno","caio"]
+print("original", nomes)
+nomes.append("daniel")
+print("atualizado", nomes)
